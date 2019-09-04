cfef994e90c9c783494aa7c6b341b5c21a533c0a
MRDGH2821 <mihirrabade@gmail.com>
2019-09-04 22:49:23 +0530

    Added task 4 code.

---
+++ C programs/task4.c
@@ -0,0 +1,10 @@
+//WRITE A C PROGRAM TO READ THE FIRST NAME AND THE LAST NAME OF THE USER IN 2 DIFFERENT VARIABLES, COMBINE THEM, AND DISPLAY THEIR FULL NAME USING A SINGLE STRING.
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  char fname[10],lname[10],name[20];
+  printf("ENter first name: ");
+  scanf("%s",fname);
+
+}
