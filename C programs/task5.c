15d14cf6953ead7e79aed83f84ea483956dea802
MRDGH2821 <mihirrabade@gmail.com>
2019-09-05 18:28:54 +0530

    Now everything is working properly.

--- C programs/task5.c
+++ C programs/task5.c
@@ -1,15 +1,16 @@
 //WRITE A C PROGRAM TO FIND THE REVERSE OF A GIVEN NUMBER.
-#include <stdio.h>
+
+ #include <stdio.h>
 int main()
 {
-		int num,rev;
+		int num=0,rev=0;
 		printf("Enter a number: ");
 		scanf("%d",&num);
 		while(num>0)
 		{
+				rev = 10*rev;
 				rev = rev + num%10;
 				num = num/10;
-				rev = 10*rev;
 		};
 		printf("Reversed number is: %d",rev);
 }
