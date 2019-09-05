a4410679c714cd519f59b48448959d558f0c33d8
MRDGH2821 <mihirrabade@gmail.com>
2019-09-05 21:24:31 +0530

    I got everything right. BUt still it gives unexpected output. Even after this code being similar to one I found online.

--- C programs/task6.c
+++ C programs/task6.c
@@ -2,30 +2,28 @@
 
 #include <stdio.h>
 #include <math.h>
+
 int main()
 {
-		int no;
+		int no,res,count=0,rem;
 		printf("Enter a number: ");
 		scanf("%d",&no);
 		const int org=no;
-		int res,count=0,rem;
-		while(no>0)
+		while(no!=0)
 		{
-				count++;
-				no=no/10;
+				++count;
+				no/=10;
 		}
 		printf("%d\n",count);
 		no=org;
 		while(no!=0)
 		{
 				rem=no%10;
-				res=res + pow(rem,count);
-				no=no/10;
+				res+= pow(rem,count);
+				no/=10;
 		}
 		if (res==org)
 				printf("True");
-
 		else
 				printf("False");
-
 }
