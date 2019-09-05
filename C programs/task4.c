2385d1b9789cf2cd5ba244deec876a3c639d259f
MRDGH2821 <mihirrabade@gmail.com>
2019-09-05 16:39:42 +0530

    This is the ultimate. This solves the question with 1 problem, i.e. no white space can be concatenated without drastically changing the result

--- C programs/task4.c
+++ C programs/task4.c
@@ -1,14 +1,17 @@
 //WRITE A C PROGRAM TO READ THE FIRST NAME AND THE LAST NAME OF THE USER IN 2 DIFFERENT VARIABLES, COMBINE THEM, AND DISPLAY THEIR FULL NAME USING A SINGLE STRING.
 #include <stdio.h>
-//#include <string.h>
+#include <string.h>
 int main()
 {
-		char fname[10],lname[10];
+		char fname[100],lname[100];
 		printf("Enter first name: ");
 		scanf("%s",fname);
 		printf("Enter last name: ");
 		scanf("%s",lname);
-		char name[]=fname lname;
-		printf("Name:%s",name);
-
+		//char w=' ';
+		//strcat(fname,&w);
+		strcat(fname,lname);
+		//char name[]=fname lname;
+		//printf("Name:%s",name);
+		puts(fname);
 }
