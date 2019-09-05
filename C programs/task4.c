e7b622a6025eeaa61c242d6be5b52f3c10fced5a
MRDGH2821 <mihirrabade@gmail.com>
2019-09-05 15:47:51 +0530

    By using this kind of declaration, the code once ran successfully. https://stackoverflow.com/a/8465854

--- C programs/task4.c
+++ C programs/task4.c
@@ -1,15 +1,14 @@
 //WRITE A C PROGRAM TO READ THE FIRST NAME AND THE LAST NAME OF THE USER IN 2 DIFFERENT VARIABLES, COMBINE THEM, AND DISPLAY THEIR FULL NAME USING A SINGLE STRING.
 #include <stdio.h>
-#include <string.h>
+//#include <string.h>
 int main()
 {
-        char fname[20],lname[10],name[20],w=32;
-        printf("Enter first name: ");
-        //scanf("%s",fname);
-        fgets(fname,20,stdin);
-        printf("Enter last name: ");
-        //scanf("%s",fname);
-        fgets(lname,10,stdin);
-        strcat(fname,lname);
-        printf("Name:%s",name);
+		char fname[10],lname[10];
+		printf("Enter first name: ");
+		scanf("%s",fname);
+		printf("Enter last name: ");
+		scanf("%s",lname);
+		char name[]=fname lname;
+		printf("Name:%s",name);
+
 }
