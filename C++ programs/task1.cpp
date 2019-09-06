e38dbe66adf9a7be94e80fddeb8358c767718361
MRDGH2821 <mihirrabade@gmail.com>
2019-09-06 16:48:48 +0530

    Code Completed. Successfully displays ASCII value of given character.

---
+++ C++ programs/task1.cpp
@@ -0,0 +1,10 @@
+//Write a C++ program to print ASCII value of a character.
+#include <iostream>
+using namespace std;
+int main()
+{
+		char c;
+		cout<<"Enter a character: ";
+		cin>>c;
+		cout<<"ASCII Value is: "<<int(c);
+}
