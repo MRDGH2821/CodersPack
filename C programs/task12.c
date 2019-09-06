4a0399e8fe392f84b4bfb8909e154090988b312a
MRDGH2821 <mihirrabade@gmail.com>
2019-09-06 15:39:26 +0530

    Code Completed. Successfully converts the given temperature according to choice.

--- C programs/task12.c
+++ C programs/task12.c
@@ -4,3 +4,28 @@
    3. EXIT.
    PERFORM THE RELEVANT OPERATION USING SWITCH CASE.
  */
+
+#include <stdio.h>
+int main()
+{
+		float f,c;
+		int ch;
+		printf("Menu:\n1.Celcius to Fahrenheit \n2.Fahrenheit to Celcius \n3.Exit\nEnter choice: ");
+		scanf("%d",&ch);
+		switch(ch)
+		{
+		case 1:  printf("Enter temperature in Celcius: ");
+				scanf("%f",&c);
+				f=1.8*c + 32;
+				printf("\nTemperature in Fahrenheit: %f",f);
+				break;
+
+		case 2:  printf("Enter temperature in Fahrenheit: ");
+				scanf("%f",&f);
+				c=(f-32)/1.8;
+				printf("\nTemperature in Celcius: %f",c);
+				break;
+
+		default: exit(0);
+		}
+}
