e982c007a6eef8ea936a30f95a124c32b8b95b76
MRDGH2821 <mihirrabade@gmail.com>
2019-09-06 15:50:13 +0530

    COde completed. Successfully finds smallest and largest number in an array.

--- C programs/task13.c
+++ C programs/task13.c
@@ -1,1 +1,30 @@
 // WRITE A C PROGRAM TO FIND THE SMALLEST AND LARGEST ELEMENT IN AN ARRAY.
+
+#include <stdio.h>
+
+int main()
+{
+		int ar[10],i;
+		printf("Enter 10 numbers:\n");
+		for (i=0; i<10; i++)
+		{
+				scanf("%d",&ar[i]);
+		}
+		int small=ar[0],large=0;
+		i=0;
+		while(i<10)
+		{
+				//condition for large
+				if (large<ar[i])
+				{
+						large=ar[i];
+				}
+				//condition for small
+				if (small>ar[i])
+				{
+						small=ar[i];
+				}
+				i++;
+		}
+		printf("Largest no: %d\nSmallest no: %d\n",large,small);
+}
