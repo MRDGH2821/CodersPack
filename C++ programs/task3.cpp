7c87644c9e80ae6d2f54bc4feca4644ee56de405
MRDGH2821 <mihirrabade@gmail.com>
2019-09-08 11:04:45 +0530

    Code completed. Successfully Checks for leap years and century leap years.

---
+++ C++ programs/task3.cpp
@@ -0,0 +1,25 @@
+//Write a C++ program to check if the given year is a leap year.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+		int year;
+		cout<<"Enter a year: ";
+		cin>>year;
+		if(year%100==0)
+		{
+				if (year%400==0)
+				{
+						cout<<"Century Leap year.\n";
+				}
+				else
+						cout<<"Not a century leap year.\n";
+		}
+		else if (year%4==0)
+				cout<<"Leap year.\n";
+		else
+				cout<<"Not a leap Year.\n";
+		return 0;
+}
