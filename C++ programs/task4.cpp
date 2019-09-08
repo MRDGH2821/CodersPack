c8e1081f912c115d19b3473e1c78b5183a1c49f1
MRDGH2821 <mihirrabade@gmail.com>
2019-09-08 11:12:55 +0530

    Code completed Successfully calculates sum of integer array.

---
+++ C++ programs/task4.cpp
@@ -0,0 +1,17 @@
+// Write a C++ program to calculate sum of all the elements of an array.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+		int ar[10];
+		cout<<"Enter 10 elements: "<<endl;
+		int sum=0;
+		for (int i=0; i<10; i++)
+		{
+				cin>>ar[i];
+				sum+=ar[i];
+		}
+		cout<<"Sum of array: "<<sum;
+}
