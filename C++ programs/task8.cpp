74128dcb29f471390231dc26996c4cce3a036cbc
MRDGH2821 <mihirrabade@gmail.com>
2019-09-08 12:19:31 +0530

    Base logic written for finding anagrams

---
+++ C++ programs/task8.cpp
@@ -0,0 +1,34 @@
+/*
+   Write a C++ program to check whether two given strings are an anagram.
+
+   Test Data:
+   Input the first String: spare
+   Input the second String: pears
+   Expected Output:
+   "spare and pears are Anagram. "
+ */
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+		char a[50],b[50];
+		cout<<"Enter string 1: ";
+		gets(a);
+		cout<<"Enter string 2: ";
+		gets(b);
+		la=strlen(a);
+		lb=strlen(b);
+		if (la==lb)
+		{
+				for(int ai=0; ai<la; ai++)
+				{
+						for (int bi=0; bi<lb; bi++)
+						{
+
+						}
+				}
+		}
+}
