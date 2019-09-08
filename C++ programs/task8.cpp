bd8b0782af5fd710ac6cb6f023b3286b71392f68
MRDGH2821 <mihirrabade@gmail.com>
2019-09-08 12:56:56 +0530

    Added all required checks. But not giving expected output

--- C++ programs/task8.cpp
+++ C++ programs/task8.cpp
@@ -19,8 +19,9 @@ int main()
 		gets(a);
 		cout<<"Enter string 2: ";
 		gets(b);
-		la=strlen(a);
-		lb=strlen(b);
+		int la=strlen(a);
+		int lb=strlen(b);
+		int ci=0;
 		if (la==lb)
 		{
 				for(int ai=0; ai<la; ai++)
@@ -28,8 +29,15 @@ int main()
 						for (int bi=0; bi<lb; bi++)
 						{
 								if (a[ai]==b[bi])
+								{
 										c[ci]=a[ai];
+										ci++;
+								}
 						}
 				}
+				if(strlen(c)==la)
+						cout<<"They are anagrams";
 		}
+		else
+				cout<<"Length must be same for anagrams."<<endl;
 }
