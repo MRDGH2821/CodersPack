14ad872751d58c5a8ea38fd6d959e0135053aa59
MRDGH2821 <mihirrabade@gmail.com>
2019-09-08 10:52:06 +0530

    Commited again, old code got commited previously.

---
+++ C++ programs/task2.cpp
@@ -0,0 +1,15 @@
+//Write a C++ program to check if the given number is even or odd.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+		int no;
+		cout<<"Enter a no.: ";
+		cin>>no;
+		if(no%2==0)
+				cout<<"Even";
+		else
+				cout<<"Odd";
+		return 0;
+}
