b5e3c859e1fc641b507108a89c1daae1ac7f86ce
MRDGH2821 <mihirrabade@gmail.com>
2019-09-21 22:54:42 +0530

    Initial commit Code successfully converts upper case characters to lower case chaacters.

---
+++ C++ programs/task10.cpp
@@ -0,0 +1,26 @@
+//Write a C++ program to convert a string to lowercase.
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+char to_lowercase(char c)
+{
+		if (c >= 'A' && c <= 'Z')
+				return c + 32;
+
+		return c;
+}
+
+int main()
+{
+		string str;
+		cout<<"Enter a string: ";
+		getline(cin,str);
+		for (char &c: str)
+		{
+				c = to_lowercase(c);
+		}
+		cout << str;
+		return 0;
+}
