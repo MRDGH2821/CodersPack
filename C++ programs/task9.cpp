7a08b9afa54b439c59ba4a382ecdb536848bda68
MRDGH2821 <mihirrabade@gmail.com>
2019-09-21 22:39:43 +0530

    Initial Commit. Code completed successfuly

---
+++ C++ programs/task9.cpp
@@ -0,0 +1,30 @@
+/*
+   Write a C++ program to convert specified days into years, weeks and days.
+
+   Note: Ignore leap year.
+   Test Data :
+   Number of days : 1329
+   Expected Output :
+   Years: 3
+   Weeks: 33
+   Days: 3
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+		int days,y,w,d;
+		cout<<"Enter number of days: ";
+		cin>>days;
+		y=days/365;
+		days=days%365;
+		w=days/7;
+		days=days%7;
+		d=days;
+		cout<<"Years: "<<y<<endl;
+		cout<<"Weeks: "<<w<<endl;
+		cout<<"Days : "<<d<<endl;
+
+}
