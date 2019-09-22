0269352def44ef7c656809b608581e1986be5e45
MRDGH2821 <mihirrabade@gmail.com>
2019-09-22 16:15:43 +0530

    Code completed

---
+++ Python programs/task8.py
@@ -0,0 +1,12 @@
+"""Write a python function to multiply all the numbers in a list."""
+
+
+def multiply(li):
+    """Multiplies all the elements in the list or tuple."""
+    prod = 1
+    for i in li:
+        prod = prod * i
+    return prod
+
+
+print("Product is:", multiply([int(i) for i in input("Enter numbers seperated by space:").split(' ')]))
