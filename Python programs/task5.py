a2ad97cf98df5ff76438ee114ee1ce39cddbbf4a
MRDGH2821 <mihirrabade@gmail.com>
2019-09-22 15:58:41 +0530

    Code completed

---
+++ Python programs/task5.py
@@ -0,0 +1,12 @@
+# Write a Python program to convert month name to the number of days.
+# January, February, March, April, May, June, July, August, September, October, November, December
+month_name = str(input("Enter month (With first letter capital): "))
+
+if month_name == "February":
+    print("No. of days: 28/29 days")
+elif month_name in ("April", "June", "September", "November"):
+    print("No. of days: 30 days")
+elif month_name in ("January", "March", "May", "July", "August", "October", "December"):
+    print("No. of days: 31 day")
+else:
+    print("Wrong month name")
