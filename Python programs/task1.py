ae520e0e7c7b521aa25d1db9a20de7d6dc0e5f04
MRDGH2821 <mihirrabade@gmail.com>
2019-09-22 14:27:46 +0530

    Code completed successfully

--- Python programs/task1.py
+++ Python programs/task1.py
@@ -1,5 +1,3 @@
-"""
-Write a Python program which accepts the radius of a circle from the user and
-compute the area.
-"""
+# WAP program which takes the radius of a circle and compute the area.
+
 print("The area is", (int(input("Enter Radius:"))**2) * 3.14)
