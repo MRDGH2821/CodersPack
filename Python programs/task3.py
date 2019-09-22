d988880fc8bc8a3f404c5c91db0e889697a9e477
MRDGH2821 <mihirrabade@gmail.com>
2019-09-22 14:40:30 +0530

    COde completed

---
+++ Python programs/task3.py
@@ -0,0 +1,2 @@
+# Write a Python function that takes a list of words and returns the length of the longest one.
+print("Longest word is:", max([str(i) for i in input("Enter words seperated by comma:").split(',')],key=len))
