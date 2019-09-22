17f1e7768a5e1845e58888d2cd1ef967c20d68fd
MRDGH2821 <mihirrabade@gmail.com>
2019-09-22 16:20:58 +0530

    Code completed

---
+++ Python programs/task9.py
@@ -0,0 +1,12 @@
+"""Write a python program to count the number of even and odd numbers from a series of numbers."""
+
+odd = 0
+even = 0
+num = [int(i) for i in input("Enter series of numbers separated by spaces:").split()]
+for j in num:
+    if j % 2 == 0:
+        even = even + 1
+    else:
+        odd = odd + 1
+
+print("Odd numbers:", odd, "\nEven numbers:", even)
