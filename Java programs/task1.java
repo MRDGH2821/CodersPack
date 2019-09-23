8065c8b8992f60b5a85bb30e34177427d9b58232
MRDGH2821 <mihirrabade@gmail.com>
2019-09-23 22:08:26 +0530

    I think the code is acceptable. It should run

--- Java programs/task1.java
+++ Java programs/task1.java
@@ -1,6 +1,18 @@
 //Write a Java program to print the area and perimeter of a circle.
 
-class circle
-{
+import java.util.Scanner;
+public class circle {
 
+/* This is my first java program.
+ * This will print 'Hello World' as the output
+ */
+
+public static void main(String [] args) {
+        int r;
+        System.out.println("Enter radius of circle:"); // prints Hello World
+        Scanner in = new Scanner(System.in);
+        r=in.nextInt();
+        System.out.println("Area of circle is:" + 3.14*r*r);
+        System.out.println("Perimeter of circle is:" + 2*3.14*r);
+}
 }
