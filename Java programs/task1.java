7d2a2a4121aeee5e887edb9a6fac1ebc7260369b
MRDGH2821 <mihirrabade@gmail.com>
2019-09-26 16:16:46 +0530

    just bkp

--- Java programs/task1.java
+++ Java programs/task1.java
@@ -1,18 +1,36 @@
-//Write a Java program to print the area and perimeter of a circle.
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+    float a,b,c,disc,root1,root2,rooti;
 
-import java.util.Scanner;
-public class circle {
+    printf("Enter the value of a,b,c (in the equation ax^2+bx+c)\n");
 
-/* This is my first java program.
- * This will print 'Hello World' as the output
- */
+    scanf("%f %f %f",&a,&b,&c);
 
-public static void main(String [] args) {
-        int r;
-        System.out.println("Enter radius of circle:"); // prints Hello World
-        Scanner in = new Scanner(System.in);
-        r=in.nextInt();
-        System.out.println("Area of circle is:" + 3.14*r*r);
-        System.out.println("Perimeter of circle is:" + 2*3.14*r);
-}
+    disc=pow(b,2)-4*a*c;
+    if(disc>0)
+    {
+        root1=(-b+sqrt(disc))/(2*a);
+        root2=(-b-sqrt(disc))/(2*a);
+    }
+    else if(disc==0)
+	{
+        root1=(-b/2*a);
+        root2=root1;
+        }
+    else if(disc<0)
+	{
+        root1=-b/(2*a);
+        root2=-b/(2*a);
+        rooti=sqrt(-disc)/(2*a);
+    	}
+    if(disc>=0)
+	{
+        printf("Root 1:%f \nRoot 2:%f",root1,root2);
+    	}
+        if(disc<0)
+	{
+        printf("Root 1: %f+%fi \nRoot 2: %f-%fi",root1,rooti,root2,rooti);
+        }
 }
