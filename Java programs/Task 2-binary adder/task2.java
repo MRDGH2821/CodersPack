cc03ac0a8ef8e9ba165abcf08550547c25ce8e56
MRDGH2821 <mihirrabade@gmail.com>
2019-10-06 00:01:55 +0530

    Added binary anumber adding function

--- Java programs/Task 2-binary adder/task2.java
+++ Java programs/Task 2-binary adder/task2.java
@@ -3,7 +3,24 @@
 import java.util.Scanner;
 
 public class task2 {
+public static String addBinary(String input0,String input1){
+        // The two input Strings, containing the binary representation of the two values:
 
+
+
+        // Use as radix 2 because it's binary
+        int number0 = Integer.parseInt(input0, 2);
+        int number1 = Integer.parseInt(input1, 2);
+
+        int sum = number0 + number1;
+        return Integer.toBinaryString(sum); //returns the answer as a binary value;
+}
 public static void main(String [] args) {
+        String n1;
+        String n2;
+        Scanner in = new Scanner(System.in);
+        n1=in.nextString();
+        n2=in.nextString();
+        System.out.println("Output = " + addBinary(n1,n2));
 }
 }
