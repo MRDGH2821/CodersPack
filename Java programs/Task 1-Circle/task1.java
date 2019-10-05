e713aec7c22aa6396ec844efac8f47cd9ca6a44b
MRDGH2821 <mihirrabade@gmail.com>
2019-10-06 00:11:12 +0530

    Code beautified

--- Java programs/Task 1-Circle/task1.java
+++ Java programs/Task 1-Circle/task1.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 public class task1 {
 
 public static void main(String [] args) {
-        int r;
-        System.out.println("Enter radius of circle:"); // prints Hello World
-        Scanner in = new Scanner(System.in);
-        r=in.nextInt();
-        System.out.println("Area of circle is:" + 3.14*r*r);
-        System.out.println("Perimeter of circle is:" + 2*3.14*r);
+	int r;
+	System.out.println("Enter radius of circle:"); // prints Hello World
+	Scanner in = new Scanner(System.in);
+	r=in.nextInt();
+	System.out.println("Area of circle is:" + 3.14*r*r);
+	System.out.println("Perimeter of circle is:" + 2*3.14*r);
 }
 }
