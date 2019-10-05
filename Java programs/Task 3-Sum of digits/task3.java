0864aa0c380b912a09d5534e4beb1434ba1e6b3c
MRDGH2821 <mihirrabade@gmail.com>
2019-10-06 00:12:54 +0530

    Code completed

---
+++ Java programs/Task 3-Sum of digits/task3.java
@@ -0,0 +1,18 @@
+//Write a Java program and compute the sum of the digits of an integer
+import java.util.Scanner;
+
+public class task1 {
+
+public static void main(String [] args) {
+	int r,sum=0;
+	System.out.println("Enter a Number:"); // prints Hello World
+	Scanner in = new Scanner(System.in);
+	r=in.nextInt();
+	while(r>0)
+	{
+		sum = sum + r%10;
+		r=r/10;
+	}
+	System.out.println("Sum of digits is = "+sum);
+}
+}
