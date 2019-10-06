53074bbdc91aed8f16a334387d2b8f52aafede4b
MRDGH2821 <mihirrabade@gmail.com>
2019-10-06 15:12:44 +0530

    Code completed

---
+++ Java programs/Task 6-NegPos/task6.java
@@ -0,0 +1,24 @@
+//Write a Java program that reads an integer and check whether it is negative, zero or positive
+
+import java.util.Scanner;
+public class task6 {
+public static void main(String[] args)
+{
+	int a;
+	Scanner in = new Scanner(System.in);
+	System.out.println("Enter a number:");
+	a = in.nextInt();
+	if (a<0)
+	{
+		System.out.println("It is negative");
+	}
+	else if(a>0)
+	{
+		System.out.println("It is positive");
+	}
+	else
+	{
+		System.out.println("It is zero");
+	}
+}
+}
