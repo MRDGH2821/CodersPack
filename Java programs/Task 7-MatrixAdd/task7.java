01685e05a7d4982a6ccfb6dde3974bb0102ec391
MRDGH2821 <mihirrabade@gmail.com>
2019-10-06 16:29:25 +0530

    Learning ho to pass arrays

--- Java programs/Task 7-MatrixAdd/task7.java
+++ Java programs/Task 7-MatrixAdd/task7.java
@@ -1,22 +1,30 @@
 //Write a Java program to add two matrices of the same size
 import java.util.scanner;
-public class task7 {
-public int accept(p[][]){
+public class task7
+{
+
+public int accept(p[][])
+{
 	Scanner in = new scanner(System.in);
-	System.out.println("Enter Matrix values: ")
+	System.out.println("Enter Matrix values: ");
 	for(int i=0; i<5; i++)
 	{
 		for(int j=0; j<5; j++)
 		{
-			System.out.print("Matrix "+i+j);
+			System.out.print("Matrix value at "+ i +"," + j );
 			p[i][j]=in.nextInt();
 		}
 	}
+	return p;
 }
 public static void main(String[] args)
 {
-	int a[5][5],b[5][5];
-
+	int a[5][5];
+	int b[5][5];
+	System.out.println("Matrix A:");
+	accept(a);
+	System.out.println("Matrix B:");
+	accept(b);
 
 
 }
