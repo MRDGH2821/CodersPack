825581485b1f29d486468868d185ebb31beee708
MRDGH2821 <mihirrabade@gmail.com>
2019-10-06 15:24:12 +0530

    Added matrix acceping function

---
+++ Java programs/Task 7-MatrixAdd/task7.java
@@ -0,0 +1,23 @@
+//Write a Java program to add two matrices of the same size
+import java.util.scanner;
+public class task7 {
+public int accept(p[][]){
+	Scanner in = new scanner(System.in);
+	System.out.println("Enter Matrix values: ")
+	for(int i=0; i<5; i++)
+	{
+		for(int j=0; j<5; j++)
+		{
+			System.out.print("Matrix "+i+j);
+			p[i][j]=in.nextInt();
+		}
+	}
+}
+public static void main(String[] args)
+{
+	int a[5][5],b[5][5];
+
+
+
+}
+}
