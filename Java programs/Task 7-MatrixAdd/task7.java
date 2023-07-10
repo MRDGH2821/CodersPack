42de34dcaa8ae9d6fbb38e4ff61d30d4bdaffb74
MRDGH2821 <mihirrabade@gmail.com>
2023-07-10 23:31:52 +0530

    fix: fix various things
    refactor(task7.java): fix class name to follow Java naming conventions
    refactor(task7.java): fix import statement for Scanner class
    refactor(task7.java): fix method signature for accept() method
    refactor(task7.java): fix indentation and formatting for better readability
    refactor(task7.java): close Scanner object after accepting matrix values
    refactor(task7.java): fix variable declarations for matrices a and b
    refactor(task7.java): fix method calls for accepting matrix values in main() method

--- Java programs/Task 7-MatrixAdd/task7.java
+++ Java programs/Task 7-MatrixAdd/task7.java
@@ -1,31 +1,29 @@
 //Write a Java program to add two matrices of the same size
-import java.util.scanner;
-public class task7
-{
-   public int accept(p[][])
-   {
-      Scanner in = new scanner(System.in);
+import java.util.Scanner;
 
-      System.out.println("Enter Matrix values: ");
-      for (int i = 0; i < 5; i++)
-      {
-         for (int j = 0; j < 5; j++)
-         {
-            System.out.print("Matrix value at " + i + "," + j);
-            p[i][j] = in.nextInt();
-         }
+public class Task7 {
+
+  public static int[][] accept(int p[][]) {
+    Scanner in = new Scanner(System.in);
+
+    System.out.println("Enter Matrix values: ");
+    for (int i = 0; i < 5; i++) {
+      for (int j = 0; j < 5; j++) {
+        System.out.print("Matrix value at " + i + "," + j + ": ");
+        p[i][j] = in.nextInt();
       }
-      return(p);
-   }
+    }
+    in.close();
+    return (p);
+  }
 
-   public static void main(String[] args)
-   {
-      int a[5][5];
-      int b[5][5];
+  public static void main(String[] args) {
+    int[][] a = new int[5][5];
+    int[][] b = new int[5][5];
 
-      System.out.println("Matrix A:");
-      accept(a);
-      System.out.println("Matrix B:");
-      accept(b);
-   }
+    System.out.println("Matrix A:");
+    accept(a);
+    System.out.println("Matrix B:");
+    accept(b);
+  }
 }
