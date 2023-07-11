89dc3ea60d3937eb960c9898a6e9ba497b48b4ef
MRDGH2821 <mihirrabade@gmail.com>
2023-07-11 18:44:21 +0530

    feat: add matrix addition function
    refactor: catch specific exception & rename variables

--- Java Programs/src/task_7_matrix_add/Task7.java
+++ Java Programs/src/task_7_matrix_add/Task7.java
@@ -1,6 +1,7 @@
 // Write a Java program to add two matrices of the same size.
 package src.task_7_matrix_add;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public final class Task7 {
@@ -15,26 +16,45 @@ public final class Task7 {
   /**
    * Accepts 5x5 Matrix Input.
    *
-   * @param matrix 5x5 Matrix variable
    * @return 5x5 Matrix filled with input values
    */
-  public static int[][] accept(final int[][] matrix) {
+  public static int[][] acceptMatrix() {
+    int[][] matrix = new int[SIZE][SIZE];
     System.out.println("Enter Matrix values: ");
-    Scanner in = new Scanner(System.in);
+    Scanner inputScan = new Scanner(System.in);
     try {
       for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
           System.out.print("Matrix value at " + i + "," + j + ": ");
-          matrix[i][j] = in.nextInt();
+          matrix[i][j] = inputScan.nextInt();
         }
       }
-    } catch (Exception e) {
+    } catch (InputMismatchException e) {
       System.out.println("Invalid input");
     } finally {
-
-      in.close();
+      inputScan.close();
     }
     return matrix;
+  }
+
+  /**
+   * Adds two 5x5 Matrices and displays them.
+   *
+   * @param matrixA
+   * @param matrixB
+   */
+  public static void addMatrix(final int[][] matrixA, final int[][] matrixB) {
+    int[][] matrixC = new int[SIZE][SIZE];
+    System.out.println("Calculating Sum of Matrix A & B:");
+    for (int i = 0; i < SIZE; i++) {
+      System.out.print("[");
+      for (int j = 0; j < SIZE; j++) {
+        matrixC[i][j] = matrixA[i][j] + matrixB[i][j];
+        System.out.print(matrixC[i][j] + " ");
+      }
+      System.out.println("]");
+    }
+
   }
 
   /**
@@ -43,12 +63,14 @@ public final class Task7 {
    * @param args
    */
   public static void main(final String[] args) {
-    int[][] matrixA = new int[SIZE][SIZE];
-    int[][] matrixB = new int[SIZE][SIZE];
 
     System.out.println("Matrix A:");
-    accept(matrixA);
+    int[][] matrixA = acceptMatrix();
+
     System.out.println("Matrix B:");
-    accept(matrixB);
+    int[][] matrixB = acceptMatrix();
+
+    System.out.println("Sum of Matrices:");
+    addMatrix(matrixA, matrixB);
   }
 }
